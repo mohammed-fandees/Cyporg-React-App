e0abc1e71648991cd6e48030baae7be03c27e0f9
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-20 20:39:32 +0300

    add footer content

--- src/sections/Footer/footer.js
+++ src/sections/Footer/footer.js
@@ -1,7 +1,12 @@
+import "./footer.css"
+
 function Footer() {
   return(
     <footer>
-      <h1>This is Footer</h1>
+      <p className="text-center text-white m-0">
+        Copyright &copy; 2036 <a href="#">Cyporg Gaming </a>
+        Company. All right reserved. <br /> Design: <a href="#">TemplateMo</a>
+      </p>
     </footer>
   );
 }
