4dc9309e2294e6401291f475d516f92c3b6931d6
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-20 17:16:55 +0300

    export the new component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MainButton } from "./MainButton/MainButton";
 export { default as Game } from "./Game/Game";
+export { default as GameMainDetails } from "./GameMainDetails/GameMainDetails";
