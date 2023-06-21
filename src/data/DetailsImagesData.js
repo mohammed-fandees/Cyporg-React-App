ccb99a8d3e509cd2b30a7826f179f9878ab8d330
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 16:07:03 +0300

    create new data files

---
+++ src/data/DetailsImagesData.js
@@ -0,0 +1,9 @@
+import imgDetails1 from "../assets/imgs/details-01.jpg"
+import imgDetails2 from "../assets/imgs/details-02.jpg"
+import imgDetails3 from "../assets/imgs/details-03.jpg"
+
+export const DetailsImagesData = [
+  {id: "img-details-1", img_url: imgDetails1, alt: "image"},
+  {id: "img-details-2", img_url: imgDetails2, alt: "image"},
+  {id: "img-details-3", img_url: imgDetails3, alt: "image"}
+]
---
+++ src/data/SideDetailsData.js
@@ -0,0 +1,6 @@
+export const SideDetailsData = [
+  { id: "slide-data-1", icon: "star", content: "4.8" },
+  { id: "slide-data-2", icon: "download", content: "2.3M" },
+  { id: "slide-data-3", icon: "server", content: "36GB" },
+  { id: "slide-data-4", icon: "gamepad", content: "Action" },
+];
