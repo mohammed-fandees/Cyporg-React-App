2fb3aa84fa1bcdcf07cec2c833485357810529ca
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 16:07:49 +0300

    create export container for data files

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+export { GamesLibraryData } from "./GamesLibraryData";
+export { MostPopularData } from "./MostPopularData";
+export { NavbarData } from "./NavbarData";
+export { SideDetailsData } from "./SideDetailsData";
+export { DetailsImagesData } from "./DetailsImagesData";
+export { RelatedGamesData } from "./RelatedGamesData";
