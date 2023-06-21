8160a29f80a0693974881b248a550418ac572071
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 11:26:09 +0300

    update navbar links references

--- src/data/NavbarData.js
+++ src/data/NavbarData.js
@@ -3,7 +3,7 @@ import profile from "../assets/imgs/profile-header.jpg"
 export const NavbarData = [
   {id: "navbar-link-1", img: false, content: "home", class: "active", reference: "/"},
   {id: "navbar-link-2", img: false, content: "browse", class: "", reference: "/browse"},
-  {id: "navbar-link-3", img: false, content: "details", class: "", reference: "/#"},
-  {id: "navbar-link-4", img: false, content: "stream", class: "", reference: "/#"},
-  {id: "navbar-link-5", img: true, img_url: profile, content: "profile", class: "rounded-pill profile", reference: "/#"},
+  {id: "navbar-link-3", img: false, content: "details", class: "", reference: "/details"},
+  {id: "navbar-link-4", img: false, content: "stream", class: "", reference: "/stream"},
+  {id: "navbar-link-5", img: true, img_url: profile, content: "profile", class: "rounded-pill profile", reference: "/profile"},
 ]
