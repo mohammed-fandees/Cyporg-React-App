34ea7a8e154f65cbf4b06ff4f557b9ec47013db0
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 16:38:25 +0300

    add component to exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as MainButton } from "./MainButton/MainButton";
 export { default as Game } from "./Game/Game";
+export { default as SectionHeader } from "./SectionHeader/SectionHeader"
 
 export { default as GameMainDetails } from "./GameMainDetails/GameMainDetails";
 export { All } from "./GameMainDetails/GameMainDetails";
