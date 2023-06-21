6e7e389e3ef1053c6a43555f9ddccc5cdcec0012
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 23:04:43 +0300

    adding browse page content

--- src/sections/Browse/BrowseContent.js
+++ src/sections/Browse/BrowseContent.js
@@ -1,8 +1,66 @@
 import "./browse-content.css";
+import { Container, SectionWrapper } from "../index";
+import { MainButton, SectionHeader, SlideCard, TopDownloadedGames, ServicesCards } from "../../components/index";
+import { SlideCardData, TopDownloadedGamesData, ServicesCardsData } from "../../data/index";
+import { MdOutlineArrowBackIos } from "react-icons/md";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 export default function BrowseContent() {
+  let items = SlideCardData.map(item => {
+    return <SlideCard key={item.id} img={item.img_url} name={item.name} type={item.type} 
+          streaming={item.streaming} rate={item.rate} downloads={item.downloads} />
+  });
+
+  let games = TopDownloadedGamesData.map(game => {
+    return <TopDownloadedGames key={game.id} name={game.name} type={game.type} rate={game.rate} downloads={game.downloads} img={game.img_url} alt={game.alt} />
+  });
+
+  let cards = ServicesCardsData.map(card => {
+    return <ServicesCards key={card.id} img={card.img_url} alt={card.alt} title={card.title} text={card.text} />
+  });
+
   return (
-    <>
-    </>
-  )
+    <main className="browse">
+      <div className="container">
+        <Container>
+          <div className="row">
+            <div className="col-lg-8">
+              <SectionWrapper class="overflow-hidden">
+                <SectionHeader title="featured" subtitle="games" class="d-flex justify-content-between">
+                  <div className="owl-nav d-flex align-items-center">
+                    <MdOutlineArrowBackIos />
+                    <MdOutlineArrowForwardIos />
+                  </div>
+                </SectionHeader>
+                <div className="row flex-nowrap">
+                  {items}
+                </div>
+              </SectionWrapper>
+            </div>
+            <div className="col-lg-4">
+              <SectionWrapper button="view all games">
+                <SectionHeader title="top" subtitle="downloaded" />
+                <ul>
+                  {games}
+                </ul>
+              </SectionWrapper>
+            </div>
+          </div>
+          <SectionHeader class="text-center my-5" title="how to start your" subtitle="live stream" />
+          <div className="row">
+            {cards}
+            <div className="col-12 text-center mt-5">
+              <MainButton reference="/profile">go to profile</MainButton>
+            </div>
+          </div>
+          <SectionWrapper button="discover all streams">
+            <SectionHeader title="Most Popular" subtitle=" live streams" />
+            <div className="row">
+              
+            </div>
+          </SectionWrapper>
+        </Container>
+      </div>
+    </main>
+  );
 }
