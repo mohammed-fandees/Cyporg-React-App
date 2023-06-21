e6dcd699dae35a5dd6585cacf0c9e37dfef12c7c
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-21 11:27:09 +0300

    add custom class to section wrapper

--- src/components/SectionWrapper/SectionWrapper.js
+++ src/components/SectionWrapper/SectionWrapper.js
@@ -6,7 +6,7 @@ export default function SectionWrapper(props) {
   if(!props.button) visibility = "d-none";
 
   return (
-    <div button={props.button} className="section-wrapper rounded-5 mt-5 position-relative">
+    <div button={props.button} className={`section-wrapper rounded-5 mt-5 position-relative ${props.class}`}>
       {props.children}
       <MainButton class={`${visibility} position-absolute section-wrapper-button`} reference="#" >
         {props.button}
