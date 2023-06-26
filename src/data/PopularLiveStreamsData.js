e7d1775cb23390e97ebf2b2894e7e2843295784f
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-26 19:27:07 +0300

    add new data files

---
+++ src/data/PopularLiveStreamsData.js
@@ -0,0 +1,99 @@
+import streamImg1 from "../assets/imgs/stream-05.jpg";
+import streamImg2 from "../assets/imgs/stream-06.jpg";
+import streamImg3 from "../assets/imgs/stream-07.jpg";
+import streamImg4 from "../assets/imgs/stream-08.jpg";
+import userAvatar1 from "../assets/imgs/avatar-01.jpg";
+import userAvatar2 from "../assets/imgs/avatar-02.jpg";
+import userAvatar3 from "../assets/imgs/avatar-03.jpg";
+import userAvatar4 from "../assets/imgs/avatar-04.jpg";
+
+export const PopularLiveStreamsData = [
+  {
+    id: "popular-live-stream-1",
+    img_url: streamImg1,
+    userImg: userAvatar1,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "6.7K",
+    gameName: "Fortnite",
+    userName: "KenganC",
+    desc: "just talking with fans",
+  },
+  {
+    id: "popular-live-stream-2",
+    img_url: streamImg2,
+    userImg: userAvatar2,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "8.3K",
+    gameName: "CS-GO",
+    userName: "LunaMa",
+    desc: "CS-GO 36 hours live stream",
+  },
+  {
+    id: "popular-live-stream-3",
+    img_url: streamImg3,
+    userImg: userAvatar3,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "14.9K",
+    gameName: "Dom Etirnal",
+    userName: "Areluwa",
+    desc: "maybe nathej allnight chillin",
+  },
+  {
+    id: "popular-live-stream-4",
+    img_url: streamImg4,
+    userImg: userAvatar4,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "28.1K",
+    gameName: "Pubg",
+    userName: "GangTm",
+    desc: "live streaming till morning",
+  },
+  {
+    id: "popular-live-stream-1",
+    img_url: streamImg3,
+    userImg: userAvatar1,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "6.7K",
+    gameName: "Fortnite",
+    userName: "Kengan Omeg",
+    desc: "just talking with fans",
+  },
+  {
+    id: "popular-live-stream-2",
+    img_url: streamImg1,
+    userImg: userAvatar2,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "8.3K",
+    gameName: "CS-GO",
+    userName: "Areluwa",
+    desc: "CS-GO 36 hours live stream",
+  },
+  {
+    id: "popular-live-stream-3",
+    img_url: streamImg4,
+    userImg: userAvatar3,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "14.9K",
+    gameName: "Dom Etirnal",
+    userName: "NewGangTeam",
+    desc: "maybe nathej allnight chillin",
+  },
+  {
+    id: "popular-live-stream-4",
+    img_url: streamImg2,
+    userImg: userAvatar4,
+    alt: "live-thumb",
+    userAlt: "user-img",
+    views: "28.1K",
+    gameName: "Pubg",
+    userName: "LahutaMalc",
+    desc: "live streaming till morning",
+  },
+];
---
+++ src/data/TopStreamersData.js
@@ -0,0 +1,36 @@
+import streamerImg1 from "../assets/imgs/avatar-01.jpg";
+import streamerImg2 from "../assets/imgs/avatar-02.jpg";
+import streamerImg3 from "../assets/imgs/avatar-03.jpg";
+import streamerImg4 from "../assets/imgs/avatar-04.jpg";
+export const TopStreamersData = [
+  {
+    id: "top-streamer-1",
+    img_url: streamerImg1,
+    alt: "streamer-img",
+    userName: "LahutaM",
+  },
+  {
+    id: "top-streamer-1",
+    img_url: streamerImg2,
+    alt: "streamer-img",
+    userName: "Kengan",
+  },
+  {
+    id: "top-streamer-1",
+    img_url: streamerImg3,
+    alt: "streamer-img",
+    userName: "Areluwa",
+  },
+  {
+    id: "top-streamer-1",
+    img_url: streamerImg4,
+    alt: "streamer-img",
+    userName: "Omeg",
+  },
+  {
+    id: "top-streamer-1",
+    img_url: streamerImg2,
+    alt: "streamer-img",
+    userName: "GangTeam",
+  },
+];
