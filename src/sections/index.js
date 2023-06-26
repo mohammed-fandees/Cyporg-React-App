d2550cf6c9e4bcbe1bfa619b42307f10de0f3cb0
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-26 19:26:35 +0300

    export stream content

--- src/sections/index.js
+++ src/sections/index.js
@@ -4,14 +4,10 @@ export { default as Container } from "./Container/Container";
 export { default as SectionWrapper } from "../components/SectionWrapper/SectionWrapper";
 export { default as SectionHeader } from "../components/SectionHeader/SectionHeader";
 export { default as Footer } from "./Footer/footer";
-
-// Home Components
 export { default as HomeContent} from "./Home/HomeContent";
 export { default as MostPopular } from "./Home/MostPopular/MostPopular";
 export { default as Card } from "../components/Card/Card";
 export { default as GamesLibrary } from "../sections/Home/GamesLibrary/GamesLibrary";
-
-// Browse Components
-
 export { default as BrowseContent } from "./Browse/BrowseContent";
 export { default as DetailsContent } from "./Details/DetailsContent";
+export { default as StreamsContent } from "./Streams/StreamsContent";
