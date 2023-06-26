8519c8334d07c37769dcbd8ac1c57bdef9ad1a05
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-26 19:29:20 +0300

    fix a mistake in writing link

--- src/data/NavbarData.js
+++ src/data/NavbarData.js
@@ -4,6 +4,6 @@ export const NavbarData = [
   {id: "navbar-link-1", img: false, content: "home", class: "active", reference: "/"},
   {id: "navbar-link-2", img: false, content: "browse", class: "", reference: "/browse"},
   {id: "navbar-link-3", img: false, content: "details", class: "", reference: "/details"},
-  {id: "navbar-link-4", img: false, content: "stream", class: "", reference: "/stream"},
+  {id: "navbar-link-4", img: false, content: "stream", class: "", reference: "/streams"},
   {id: "navbar-link-5", img: true, img_url: profile, content: "profile", class: "rounded-pill profile", reference: "/profile"},
 ]
