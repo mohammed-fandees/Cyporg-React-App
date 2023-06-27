ddedcf49b4f4997fa506ba7ba72c84ee5f8a8a7f
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-06-27 05:39:05 +0300

    add prop => display inline block

--- src/components/MainButton/MainButton.js
+++ src/components/MainButton/MainButton.js
@@ -3,7 +3,7 @@ import "./main-button.css"
 export default function MainButton(props) {
   return(
     <div className="main-button">
-      <a className={`main-btn rounded-pill text-white smooth px-4 text-capitalize ${props.class}`} href={props.reference}>
+      <a className={`main-btn rounded-pill text-white smooth px-4 text-capitalize d-inline-block ${props.class}`} href={props.reference}>
         {props.children}
       </a>
     </div>
